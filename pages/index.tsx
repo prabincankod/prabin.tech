6ee6739e6e0676fb432661f0d3b65d91fbb4db27
Protam.js <prabinsubedi2016@gmail.com>
2022-01-17 12:25:52 +0545

    theme for status bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,20 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Header from "../components/Header";
 import Body from "../components/Body";
+import { useColorMode } from "@chakra-ui/react";
 const Home: NextPage = () => {
+  const { colorMode } = useColorMode();
+  const isDark = colorMode === "dark";
   return (
     <>
       <Head>
         <title>Prabin Subedi:writes alot of javascript</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Prabin Subedi:writes alot of javascript"
+        />
+        <meta name="theme-color" content={isDark ? "#1A202C" : "#FFF"} />
       </Head>
       <Header />
       <Body />
